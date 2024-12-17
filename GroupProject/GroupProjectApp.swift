7c2e77fe641814ebd4a76b94cd4fa3dfc5a5f3c3
Tom Swope <tjswope@gmail.com>
2024-12-17 14:47:52 -0500

    Initial Commit

---
+++ GroupProject/GroupProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroupProjectApp.swift
+//  GroupProject
+//
+//  Created by Swope, Thomas on 12/17/24.
+//
+
+import SwiftUI
+
+@main
+struct GroupProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
